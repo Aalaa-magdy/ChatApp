7d464eb1bc9a1725180613db8a2e47e3e0ad629c
Aalaa-magdy <am7371494@gmail.com>
2026-01-24 21:03:16 +0200

    handle the auth page

--- frontend/app/(auth)/welcome.tsx
+++ frontend/app/(auth)/welcome.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
 
 const welcome = () => {
   return (
-    <View>
-      <Text>welcome</Text>
-    </View>
+     <ScreenWrapper showPattern={true} style={{paddingHorizontal:20}}>
+        <Text>Welcome Screen</Text>
+     </ScreenWrapper>
   )
 }
 
