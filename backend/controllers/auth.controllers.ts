2bd4ac135da7dcf8db3807895ad62f7f518dddf2
Aalaa-magdy <am7371494@gmail.com>
2026-02-04 12:03:17 +0200

    finish auth backend

--- backend/controllers/auth.controllers.ts
+++ backend/controllers/auth.controllers.ts
@@ -1,8 +1,65 @@
+import type { Request, Response } from "express"
 
-export const registerUser = async()=>{
+import User from "../models/user.ts";
+import bcrypt from "bcryptjs";
+import user from "../models/user.ts";
+import { generateToken } from "../utils/token.ts";
+
+export const registerUser = async(req:Request , res:Response):Promise<void>=>{
+        const {email, password, name, avatar} = req.body;
+        try{
+          let user = await User.findOne({email});
+          if(user){
+            res.status(400).json({sucess:false , message: "User already exists"});
+            return;
+          }
+          user = new User({email, password, name, avatar:avatar || ""});
+          await user.save();
+          res.status(201).json({message: "User registered successfully"});
+          // hash the password
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(password, salt);
+          // save the user
+          await user.save();
+
+         //generate a token
+         const token = generateToken(user);
+        }
+          
+      
+       
+        catch(error){
+            console.error("Error registering user", error);
+            res.status(500).json({message: "Internal server error"});
+        }
+       
 
 }
 
-export const loginUser = async()=>{
-    
+export const loginUser = async(req:Request , res:Response):Promise<void>=>{
+    const {email, password} = req.body;
+    try{
+      const user = await User.findOne({email});
+      if(!user){
+        res.status(400).json({sucess:false , message: "User not found"});
+        return;
+      }
+      // compare the password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch){
+        res.status(400).json({sucess:false , message: "Invalid password"});
+        return;
+    }
+     //generate a token
+     const token = generateToken(user);
+    }
+      
+  
+   
+    catch(error){
+        console.error("Error registering user", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+   
+
 }
