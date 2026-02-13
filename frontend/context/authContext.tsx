9feb152ab8831b5ea203cf789378f11088cb6c0c
Aalaa-magdy <am7371494@gmail.com>
2026-02-13 17:11:45 +0200

    context auth

--- frontend/context/authContext.tsx
+++ frontend/context/authContext.tsx
@@ -1,5 +1,11 @@
-import {UserProps, type AuthContextProps } from "@/types";
+import {DecodedTokenProps, UserProps, type AuthContextProps } from "@/types";
+import { useRouter } from "expo-router";
 import { createContext, useState,ReactNode } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
+import { login } from "@/services/authService";
+
+
 
 export const AuthContext = createContext <AuthContextProps>({
      token: null,
@@ -13,6 +19,26 @@ export const AuthContext = createContext <AuthContextProps>({
 export const AuthProvider = ({children}:{children:ReactNode})=>{
     const [token , setToken] = useState<string |null> (null);
     const [user , setUser] = useState<UserProps | null>(null);
+    const router = useRouter();
+
+    const updateToken = async(token:string)=>{ 
+        if(token){
+            setToken(token);
+            await AsyncStorage.setItem("token", token);
+            const decoded = jwtDecode<DecodedTokenProps>(token);
+            setUser(decoded.user);
+        }
+    }
+
+    const signIn = async(email:string, password:string)=>{
+        try{
+            const response = await login(email, password);
+            await updateToken(response.token);
+        }
+        catch(error){
+            console.error("Error signing in", error);
+        }
+    }
 
      
 }
